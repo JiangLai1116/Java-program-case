145a811799dee947dda8e329ad06f500ade5719e
JiangLai <Jactil777@gmail.com>
2024-07-26 22:34:38 +0800

    project initial commit

---
+++ src/main/java/com/jactil/javaprogramcase/JavaProgramCaseApplication.java
@@ -0,0 +1,13 @@
+package com.jactil.javaprogramcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProgramCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProgramCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jactil/javaprogramcase/JavaProgramCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jactil.javaprogramcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProgramCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
